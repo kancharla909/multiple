24f607e60074ff3eb2f056f0e904a991810eafc8
jpark132 <andyjoonpark@gmail.com>
2016-12-05 23:31:17 -0500

    adding 2 new projects to one repo

---
+++ Hello1/src/main/java/com/joon/Mytest.java
@@ -0,0 +1,8 @@
+package com.joon;
+
+public class Mytest {
+
+	public static void sayHello1() {
+		System.out.println("This is the first hello");
+	}
+}
---
+++ Hello2/src/main/java/com/joon2/App.java
@@ -0,0 +1,18 @@
+package com.joon2;
+import com.joon.Mytest;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+     sayHello();
+     Mytest.sayHello1();
+    	System.out.println( "Hello World!" );
+    }
+    public static void sayHello(){
+    	System.out.println("This is the second hello");
+    }
+}
